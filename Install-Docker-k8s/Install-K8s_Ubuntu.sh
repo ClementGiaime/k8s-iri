2216df42743b1d230a7887b48526d7778833b403
ClementGiaime <clemgiaime@orange.fr>
2017-12-06 16:01:41 +0100

    Update Install-K8s_Ubuntu.sh

--- Install-Docker-k8s/Install-K8s_Ubuntu.sh
+++ Install-Docker-k8s/Install-K8s_Ubuntu.sh
@@ -2,7 +2,7 @@
 # Installation de Docker 1.13.1 #
 #################################
 sudo apt-get update
-sudo apt-get install --no-install-recommends apt-transport-https curl software-properties-common
+sudo apt-get install --no-install-recommends apt-transport-https curl software-properties-common -y
 sudo apt-get install -y --no-install-recommends linux-image-extra-$(uname -r) linux-image-extra-virtual
 curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add -
 sudo add-apt-repository "deb https://packages.docker.com/1.13/apt/repo/ ubuntu-$(lsb_release -cs) main"
