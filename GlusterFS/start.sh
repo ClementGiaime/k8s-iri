18e4dc1dff1c144518213804adbd50e559742be4
ClementGiaime <clemgiaime@orange.fr>
2018-01-11 22:51:30 +0100

    final

--- GlusterFS/start.sh
+++ GlusterFS/start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-function Check_return {
-if [ $1 != 0 ]
-then
-  exit 1
-fi
-}
-
-
-
+#Check all pod Gluster as running
 echo -n "All pod start  "
 count=0
 while [ $count != 2 ]
@@ -39,23 +31,23 @@ get=`kubectl get pod -o wide | grep gluster-2-pod`
 read name ready  status restarts age ip node <<< `echo $get`
 gluster2ip=$ip
 
-
 #Create repo /srv/gluster/  [ PostgreSQL, Mongo, GitBucket ]
-kubectl exec -ti gluster-1-pod -- bash -c "mkdir /srv/gluster/PostgreSQL /srv/gluster/Mongo /srv/gluster/GitBucket /srv/gluster/Redis"
-kubectl exec -ti gluster-2-pod -- bash -c "mkdir /srv/gluster/PostgreSQL /srv/gluster/Mongo /srv/gluster/GitBucket /srv/gluster/Redis"
-
+kubectl exec -ti gluster-1-pod -- bash -c "mkdir /srv/gluster/PostgreSQL /srv/gluster/Mongo /srv/gluster/GitBucket /srv/gluster/Redis /srv/gluster/GitLab /srv/gluster/RocketChat" > /dev/null
+kubectl exec -ti gluster-2-pod -- bash -c "mkdir /srv/gluster/PostgreSQL /srv/gluster/Mongo /srv/gluster/GitBucket /srv/gluster/Redis /srv/gluster/GitLab /srv/gluster/RocketChat" > /dev/null
 
 #Add resolv on /etc/hosts
 kubectl exec -ti gluster-1-pod -- bash -c "gluster peer probe $gluster2ip"
-#Check_return $?
+
 kubectl exec -ti gluster-1-pod -- bash -c "gluster volume create volume-PostgreSQL replica 2 $gluster1ip:/srv/gluster/PostgreSQL $gluster2ip:/srv/gluster/PostgreSQL force"
 kubectl exec -ti gluster-1-pod -- bash -c "gluster volume create volume-Mongo replica 2 $gluster1ip:/srv/gluster/Mongo $gluster2ip:/srv/gluster/Mongo force"
-kubectl exec -ti gluster-1-pod -- bash -c "gluster volume create volume-GitBucket replica 2 $gluster1ip:/srv/gluster/GitBucket $gluster2ip:/srv/gluster/GitBucket force"
-kubectl exec -ti gluster-1-pod -- bash -c "gluster volume create volume-Redis replica 2 $gluster1ip:/srv/gluster/Redis $gluster2ip:/srv/gluster/Redis force"
-#Check_return $?
+kubectl exec -ti gluster-1-pod -- bash -c "gluster volume create volume-RocketChat replica 2 $gluster1ip:/srv/gluster/RocketChat $gluster2ip:/srv/gluster/RocketChat force"
+kubectl exec -ti gluster-1-pod -- bash -c "gluster volume create volume-GitLab replica 2 $gluster1ip:/srv/gluster/GitLab $gluster2ip:/srv/gluster/GitLab force"
+
 
 kubectl exec -ti gluster-1-pod -- bash -c "gluster volume start volume-PostgreSQL"
 kubectl exec -ti gluster-1-pod -- bash -c "gluster volume start volume-Mongo"
-kubectl exec -ti gluster-1-pod -- bash -c "gluster volume start volume-GitBucket"
-kubectl exec -ti gluster-1-pod -- bash -c "gluster volume start volume-Redis"
-#Check_return $?
+kubectl exec -ti gluster-1-pod -- bash -c "gluster volume start volume-RocketChat"
+kubectl exec -ti gluster-1-pod -- bash -c "gluster volume start volume-GitLab"
+
+kubectl exec -ti gluster-1-pod -- bash -c "gluster volume set volume-RocketChat storage.owner-gid 999"
+kubectl exec -ti gluster-1-pod -- bash -c "gluster volume set volume-RocketChat storage.owner-uid 999"
